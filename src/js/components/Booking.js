432eb738b14c7b56f49b9a694ed880025fcbc7c1
N1Anoobis <mrbond84@hotmail.com>
2020-04-12 18:23:21 +0200

    method dinamicPeopleOrder in class Booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -12,7 +12,7 @@ class Booking {
     const thisBooking = this;
     thisBooking.render(element, tables);
     thisBooking.initWidgets();
-
+    thisBooking.dinamicPeopleOrder();
   }
 
   render(element, tables) {
@@ -30,10 +30,38 @@ class Booking {
     thisBooking.dom = {};
     thisBooking.dom.wrapper = bookingWrapper;
     thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
- 
+
+
+
     thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
     thisBooking.dom.dataPicker = bookingWrapper.querySelector(select.widgets.datePicker.wrapper);
     thisBooking.dom.hourPicker = bookingWrapper.querySelector(select.widgets.hourPicker.wrapper);
+    thisBooking.dom.orderConfirmation = bookingWrapper.querySelector('.order-confirmation');
+    // console.log(document.querySelector('.booking-form'))
+  }
+
+  dinamicPeopleOrder() {
+    const thisBooking = this;
+    ///listiner to take current value of people wiget
+    let input = thisBooking.dom.peopleAmount.querySelector('input');
+    // console.log(input.value);
+    thisBooking.dom.peopleAmount.addEventListener('click', function () {
+    
+      const remove = document.querySelector('.booking-form').querySelectorAll('.order-confirmation');
+      // console.log(remove);
+      for (const elem of remove) {
+        elem.parentNode.removeChild(elem);
+      }
+      // document.querySelector('.booking-form').removeChild(thisBooking.dom.orderConfirmation);
+
+
+      for (let index = 0; index < input.value; index++) {
+        //clone the dammn thing
+        const cloned = thisBooking.dom.orderConfirmation.cloneNode(true);
+
+        document.querySelector('.booking-form').appendChild(cloned);
+      }
+    });
   }
 
   initWidgets() {

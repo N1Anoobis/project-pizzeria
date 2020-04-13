d3f9e2d8469bad8011f4e32b409ea41f766a37dc
N1Anoobis <mrbond84@hotmail.com>
2020-04-13 12:23:55 +0200

    Add simple validation and fetch

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,6 @@
 import {
   select,
+  settings,
   templates
 } from '../settings.js';
 import utils from '../utils.js';
@@ -36,31 +37,83 @@ class Booking {
     thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
     thisBooking.dom.dataPicker = bookingWrapper.querySelector(select.widgets.datePicker.wrapper);
     thisBooking.dom.hourPicker = bookingWrapper.querySelector(select.widgets.hourPicker.wrapper);
+    thisBooking.dom.bookingForm = bookingWrapper.querySelector('.booking-form');
     thisBooking.dom.orderConfirmation = bookingWrapper.querySelector('.order-confirmation');
-    // console.log(document.querySelector('.booking-form'))
+    thisBooking.dom.inputName = bookingWrapper.querySelector('.input-name');
+
+
   }
 
   dinamicPeopleOrder() {
     const thisBooking = this;
     ///listiner to take current value of people wiget
     let input = thisBooking.dom.peopleAmount.querySelector('input');
-    // console.log(input.value);
+
     thisBooking.dom.peopleAmount.addEventListener('click', function () {
-    
-      const remove = document.querySelector('.booking-form').querySelectorAll('.order-confirmation');
-      // console.log(remove);
+      //remove all inputs
+      const remove = document.querySelector('.input-name').querySelectorAll('input');
       for (const elem of remove) {
         elem.parentNode.removeChild(elem);
       }
-      // document.querySelector('.booking-form').removeChild(thisBooking.dom.orderConfirmation);
-
-
+      // display correct number of inputs
       for (let index = 0; index < input.value; index++) {
-        //clone the dammn thing
-        const cloned = thisBooking.dom.orderConfirmation.cloneNode(true);
-
-        document.querySelector('.booking-form').appendChild(cloned);
+        let inputName = document.createElement('input');
+        thisBooking.dom.inputName.appendChild(inputName);
       }
+    });
+    //listiner for submit
+    thisBooking.dom.bookingForm.addEventListener('submit', function (e) {
+      e.preventDefault();
+      thisBooking.dom.guestsNames = document.querySelector('.input-name').querySelectorAll('input');
+      // event input to check current value of input
+      const checkAllInputs = document.querySelector('.input-name').querySelectorAll('input');
+
+      // simple validation
+      for (const checkInput of checkAllInputs) {
+
+        let guestName = checkInput.value;
+
+        checkInput.classList.remove('error');
+
+        checkInput.classList.remove('error');
+        if (guestName.length < 5) {
+
+          checkInput.classList.add('error');
+          return;
+        }
+      }
+
+
+      const url = settings.db.url + '/' + settings.db.order;
+
+      const payload = {
+        guests: [],
+      };
+      for (const guest of thisBooking.dom.guestsNames) {
+        payload.guests.push(guest.value);
+      }
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      };
+      fetch(url, options)
+        .then(function (response) {
+          console.log('response', response);
+        }).then(function (parsedResponse) {
+          console.log('parsedResponse', parsedResponse);
+        });
+
+      //clearing input list after submiting order
+      const remove = document.querySelector('.input-name').querySelectorAll('input');
+      for (const elem of remove) {
+        elem.parentNode.removeChild(elem);
+      }
+      let inputName = document.createElement('input');
+      thisBooking.dom.inputName.appendChild(inputName);
+      input.value = 1;
     });
   }
 
